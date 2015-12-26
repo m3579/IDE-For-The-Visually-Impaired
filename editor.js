7bd9e67f7c1b96f0be322a9a0c202891fa1b327f
M3579 <mihirkasmalkar@gmail.com>
2015-12-26 15:35:00 -0800

    Made decisions about architecture, led to major refactoring in code; see engineering notebook for date 12/26/15

--- editor.js
+++ editor.js
@@ -2,40 +2,52 @@
 
 $(document).ready(
 	function() {
-		// Many of these will have to change according to the parser
-		
-		var previousText = "";
-		var currentWord = "";
-
-		var symbols = "!@#$%^&*()-=_+[]\{};':\",./<>?`~";
-
-		var wordTerminators = " \t\n";
-		
-		var parserType = DummyParser;
-		
-		if (typeof parserType === "undefined") {
-			throw new ReferenceError("The parser " + parserType + " does not exist");
-		}
-		
-		var parser = new DummyParser();
-
-		$("#editor").on("input", function(event) {
-			var currentText = $("#editor").val();
-	
-			var tokens = parser.parse(text);
-			
-			if (previousText.length < currentText.length) {
-				var newLetter = parser.getNewLetter();
-				speak(newLetter);
-			}
-			else if (previousText.length > currentText.length) {
-				speak("Delete");
-			}
-			else {
-				speak("An event was unnecessarily fired");
-			}
-			
-			previousText = currentText;
-		});
+		startInitChainReaction();
 	}
 );
+
+function startInitChainReaction() {
+	initCompiler();
+}
+
+function initCompiler() {
+	var TERMINATOR_CHARS = "";
+	
+	$.get("/editor/compiler", {"language": "keyboardcode"},
+		function (data) {
+			var grammar = JSON.parse(data);
+			
+			TERMINATOR_CHARS = grammar["terminator-chars"];
+			
+			initEditor();
+		}
+	);
+}
+
+function initEditor() {
+	var previousText = $("#editor").val();
+		
+	$("#editor").on("input", function(event) {
+		var currentText = $("#editor").val();
+	
+		if (previousText.length < currentText.length) {
+			var newLetter = currentText[currentText.length - 1];
+			speak(newLetter);
+		}
+		else if (previousText.length > currentText.length) {
+			speak("Delete");
+		}
+		else {
+			speak("An event was unnecessarily fired");
+		}
+		
+		previousText = currentText;
+		
+		endInitChainReaction();
+	});
+
+}
+
+function endInitChainReaction() {
+	speak("Ready");
+}
